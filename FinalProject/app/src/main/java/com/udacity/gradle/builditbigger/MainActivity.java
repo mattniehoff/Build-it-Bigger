0ee4a4c2a9a4fe6749f0c13f05dc78a6cebe154a
Matt Niehoff <nieho003@umn.edu>
2018-10-13 17:18:45 -0500

    connects to enpointsasynctask and gdc

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,8 +1,10 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -48,11 +50,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Intent intent = new Intent(this, JokeActivity.class);
-        Joker joker = new JavaJoker();
-        intent.putExtra(JokeActivity.JOKE_EXTRA, joker.getJoke());
-        startActivity(intent);
+//        Intent intent = new Intent(this, JokeActivity.class);
+//        Joker joker = new JavaJoker();
+//        intent.putExtra(JokeActivity.JOKE_EXTRA, joker.getJoke());
+//        startActivity(intent);
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
     }
-
-
 }
