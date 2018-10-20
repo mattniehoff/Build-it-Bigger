691a7901f9ef6ddf66f69bec4963ee0bf79ab860
Matt Niehoff <matt.j.niehoff@gmail.com>
2018-10-20 14:14:44 -0500

    Finishes implementing changes of free version into paid version.

--- FinalProject/app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ FinalProject/app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -20,6 +21,19 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button button = root.findViewById(R.id.get_joke_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tellJoke();
+            }
+        });
+
         return root;
+    }
+
+    public void tellJoke() {
+        new EndpointsAsyncTask().execute(this);
     }
 }
