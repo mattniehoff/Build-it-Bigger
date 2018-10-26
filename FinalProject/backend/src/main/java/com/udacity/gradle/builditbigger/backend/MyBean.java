825e62d42165a938be76458586db2df6371dddf3
Matt Niehoff <matt.j.niehoff@gmail.com>
2018-10-26 10:57:40 -0500

    Moves joker out of MyBean class.

--- FinalProject/backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyBean.java
+++ FinalProject/backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyBean.java
@@ -1,20 +1,12 @@
 package com.udacity.gradle.builditbigger.backend;
 
-import com.mattniehoff.jokes.JavaJoker;
-import com.mattniehoff.jokes.Joker;
-
 /** The object model for the data we are sending through endpoints */
 public class MyBean {
 
     private String myData;
-    private Joker joker;
 
     public String getData() {
-        if (joker == null) {
-            joker = new JavaJoker();
-        }
-
-        return joker.getJoke();
+        return myData;
     }
 
     public void setData(String data) {
--- FinalProject/backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ FinalProject/backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -3,6 +3,8 @@ package com.udacity.gradle.builditbigger.backend;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
+import com.mattniehoff.jokes.JavaJoker;
+import com.mattniehoff.jokes.Joker;
 
 import javax.inject.Named;
 
@@ -18,18 +20,12 @@ import javax.inject.Named;
 )
 public class MyEndpoint {
 
-//    /** A simple endpoint method that takes a name and says Hi back */
-//    @ApiMethod(name = "sayHi")
-//    public MyBean sayHi(@Named("name") String name) {
-//        MyBean response = new MyBean();
-//        response.setData("Hi, " + name);
-//
-//        return response;
-//    }
-
     @ApiMethod(name  = "getJoke")
     public MyBean getJoke() {
+        Joker joker = new JavaJoker();
+
         MyBean response = new MyBean();
+        response.setData(joker.getJoke());
         return response;
     }
 
